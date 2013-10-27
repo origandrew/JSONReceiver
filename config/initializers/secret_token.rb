66c2e17a5d7e6a4800f5e8be79a233b51fc19a44
Andrew Tan <andrew.tan@sv.cmu.edu>
2013-10-26 22:57:57 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JSONReceiver::Application.config.secret_token = '1269474589907571b1439e11f1a6642e2498b1a7a0369a2d2c78383ad19e65726361f6578519afc841ec50582d5059317270a1c702677846f9fe5bf1be5d93fe'
